fca7bd8bd460b0395341fee18320aac18a89a0ea
Philip Nguyen <philip.nguyen@encora.com>
2021-08-19 10:38:41 -0400

    add metric cards to assessment layout.

--- src/components/EogAssessment.tsx
+++ src/components/EogAssessment.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import MetricSelectInput from '../Features/Metric/MetricSelectInput';
+import RenderMetricCards from '../Features/Metric/RenderMetricCards';
 
 const EogAssessment = () => (
   <Grid container>
-    <MetricSelectInput />
+    <Grid item xs={12}>
+      <MetricSelectInput />
+    </Grid>
+    <Grid item xs={12}>
+      <RenderMetricCards />
+    </Grid>
   </Grid>
 );
 
